32ac1a5eab8c8679be50679409aa655c28dd01ca
Larry Jones <larry.jones@reveal-energy.com>
2020-01-01 17:31:50 -0600

    Write summarize()

--- euro.py
+++ euro.py
@@ -2,11 +2,11 @@
 
 import fractions
 
-from thinkbayes import Suite
+import thinkbayes
 import thinkplot
 
 
-class Euro(Suite):
+class Euro(thinkbayes.Suite):
     """Models a solution to the Euro problem."""
 
     def likelihood(self, data, hypothesis):
@@ -33,11 +33,28 @@ def main():
     for datum in dataset:
         suite.update(datum)
 
-    print(f'Mean={suite.mean()}')
+    summarize(suite)
 
     thinkplot.preplot(1)
     thinkplot.plot_pmf(suite)
     thinkplot.save(root='euro', xlabel='Probability of heads', ylabel='Probability')
+
+
+def summarize(suite):
+    """Summarize the suite using different point values.
+
+    Args:
+          suite(Suite): The Suite to summarize.
+    """
+    print(f'Summary of suite, {suite.name}')
+    print(f'  MLE (Most Likely Estimate)={suite.maximum_likelihood()}')
+    print(f'  Mean={int(suite.mean())}')
+
+    print(f'  5th percentile={thinkbayes.percentile(suite, 5)}')
+    print(f'  Median={thinkbayes.percentile(suite, 50)}')
+    print(f'  95th percentile={thinkbayes.percentile(suite, 95)}')
+
+    print(f'  Credible interval={suite.credible_interval()}')
 
 
 if __name__ == '__main__':
